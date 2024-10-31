4ad21f4bb62c7c558b394cf1ae1a5332e8d7d5a2
smartini <saverio.martini@he-arc.ch>
2024-10-31 16:08:56 +0100

    add OrderDataMapper

--- src/main/java/ch/hearc/ig/orderresto/persistence/OrderDataMapper.java
+++ src/main/java/ch/hearc/ig/orderresto/persistence/OrderDataMapper.java
@@ -1,24 +1,66 @@
 package ch.hearc.ig.orderresto.persistence;
 
+import ch.hearc.ig.orderresto.business.Customer;
 import ch.hearc.ig.orderresto.business.Order;
 import ch.hearc.ig.orderresto.service.DbUtils;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class OrderDataMapper {
 
-
-
-    public void insert(Order order) throws SQLException{
+    public void insert(Order currentOrder) {
         try (Connection db = DbUtils.getConnection();
              PreparedStatement dbStatement = db.prepareStatement(
-                     "INSERT INTO COMMANDE (fk_client, fk_resto,a_emporter,quand) VALUES (3,1,'O',SYSDATE)"
-             );
+                     "INSERT INTO COMMANDE (fk_client, fk_resto, a_emporter, quand) VALUES (?, ?, ?, SYSDATE)"
+             )) {
 
-        ) {
-            dbStatement.executeUpdate();
+            dbStatement.setLong(1, currentOrder.getCustomer().getId());
+            dbStatement.setLong(2, currentOrder.getRestaurant().getId());
+            dbStatement.setString(3, currentOrder.getTakeAway() ? "O" : "N"); // Assuming 'O' for take-away, 'N' otherwise
+
+
+                throw new RuntimeException("Insertion failed, no rows affected.");
+            } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+
     }
+
+    public Set<Order> findAllOrdersByCustomer(Customer customer) throws SQLException {
+        Connection db = DbUtils.getConnection();
+        try (PreparedStatement dbStatement = db.prepareStatement(
+                     "SELECT * FROM COMMANDE WHERE fk_client = ?"
+             )) {
+            dbStatement.setLong(1, customer.getId());
+            ResultSet dbResult = dbStatement.executeQuery();
+            Set<Order> ordersFound = new HashSet<>();
+            while (dbResult.next()) {
+                Order currentOrder = new Order(
+                        dbResult.getLong("numero"),
+                        customer,
+                        findRestaurantbyID(dbResult.getLong("fk_resto")), // TODO: restaurant
+                        dbResult.getString("a_emporter").equals("O"), // Assuming 'O' for take-away, 'N' otherwise
+                        dbResult.getTimestamp("quand").toLocalDateTime()
+                );
+                ordersFound.add(currentOrder);
+            }
+            return ordersFound;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+    }
+
+
+
 }
+
+
