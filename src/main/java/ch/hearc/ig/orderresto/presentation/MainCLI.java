cfa0d1a4b8f1b1e50467c64ef65f620441df67a8
Julien Dagon <julien.dagon@he-arc.ch>
2024-11-03 20:13:08 +0100

    Display customer orders with price

--- src/main/java/ch/hearc/ig/orderresto/presentation/MainCLI.java
+++ src/main/java/ch/hearc/ig/orderresto/presentation/MainCLI.java
@@ -1,10 +1,6 @@
 package ch.hearc.ig.orderresto.presentation;
 
-import ch.hearc.ig.orderresto.business.Order;
-import ch.hearc.ig.orderresto.business.Product;
 //import ch.hearc.ig.orderresto.persistence.FakeDb;
-import ch.hearc.ig.orderresto.persistence.OrderDataMapper;
-import ch.hearc.ig.orderresto.persistence.ProductDataMapper;
 
 import java.sql.SQLException;
 
@@ -27,11 +23,12 @@ public class MainCLI extends AbstractCLI {
         OrderCLI orderCLI = new OrderCLI();
         if (userChoice == 1) {
             orderCLI.createNewOrder();
-        } else {
-            Order existingOrder = orderCLI.selectOrder();
+        } else if (userChoice == 2) {
+            orderCLI.displayOrders();
+            /*Order existingOrder = orderCLI.selectOrder();
             if (existingOrder != null) {
                 orderCLI.displayOrder(existingOrder);
-            }
+            }*/
         }
         this.run();
     }
